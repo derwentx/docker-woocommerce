a1cbccc81dfc1c4c556068e1060272eb9b1121e5
derwentx <derwentx@gmail.com>
2018-08-14 14:56:50 +1000

    installs plugins and sample data from env

--- docker-entrypoint-woocommerce.sh
+++ docker-entrypoint-woocommerce.sh
@@ -35,14 +35,34 @@ if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 
         # only install wp if we have environment-supplied configuration values
         if [ "$haveConfig" ]; then
-            : "${WORDPRESS_SITE_URL:=http://localhost}"
+            : "${WORDPRESS_URL:=http://localhost}"
             : "${WORDPRESS_SITE_TITLE:=API Test}"
             : "${WORDPRESS_ADMIN_USER:=admin}"
             : "${WORDPRESS_ADMIN_PASSWORD:=admin}"
             : "${WORDPRESS_ADMIN_EMAIL:=admin@example.com}"
+            : "${WORDPRESS_ACTIVE_THEME:=}"
+            : "${SHELL:=/bin/sh}"
 
-            # Run the wp command as www-data
-            su --shell="${SHELL:='/bin/sh'}" -c "wp core install --url=\"$WORDPRESS_SITE_URL\" --title=\"$WORDPRESS_SITE_TITLE\" --admin_user=\"$WORDPRESS_ADMIN_USER\" --admin_password=\"$WORDPRESS_ADMIN_PASSWORD\" --admin_email=\"$WORDPRESS_ADMIN_EMAIL\"" www-data
+            # 
+            as_web_user() {
+                su $user --shell $SHELL -c "$*"
+            }
+
+            # Run the wp commands as web-user
+            # This part has some inspiration from https://github.com/autopilotpattern/wordpress/blob/master/bin/prestart.sh
+            as_web_user "wp core install --url=\"$WORDPRESS_SITE_URL\" --title=\"$WORDPRESS_SITE_TITLE\" --admin_user=\"$WORDPRESS_ADMIN_USER\" --admin_password=\"$WORDPRESS_ADMIN_PASSWORD\" --admin_email=\"$WORDPRESS_ADMIN_EMAIL\""
+            if [ -n "$WORDPRESS_ACTIVE_THEME" ]; then
+                as_web_user "wp theme activate \"$WORDPRESS_ACTIVE_THEME\""
+            fi
+
+            # TODO: install plugins from env variable and ensure woocommerce
+            as_web_user "wp plugin install woocommerce --activate"
+
+            if [ -n "$WOOCOMMERCE_TEST_DATA" ] && [ ! -f "sample_products.xml" ]; then
+                as_web_user "wp plugin install wordpress-importer --activate"
+                as_web_user "curl -OL https://raw.githubusercontent.com/woocommerce/woocommerce/master/sample-data/sample_products.xml"
+                as_web_user "wp import sample_products.xml --authors=create"
+            fi
         fi
     )
 fi
